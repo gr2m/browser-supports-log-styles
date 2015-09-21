603cb63d18f581a74cc3af757d45440ffc4bde8c
Gregor Martynus <gregor@martynus.net>
2015-09-21 18:04:37 +0200

    test: initial version

---
+++ tests/index.js
@@ -0,0 +1,90 @@
+var test = require('tape')
+var browserSupportsLogStyles = require('../index')
+
+// simulate browser environment
+function resetBrowserEnvironment (options) {
+  global.window = options.window || {
+    console: {}
+  }
+  global.document = options.document || {
+    documentElement: {
+      style: {}
+    }
+  }
+  global.navigator = options.navigator || {
+    userAgent: ''
+  }
+}
+
+test('browserSupportsLogStyles default', function (t) {
+  resetBrowserEnvironment({})
+
+  t.is(browserSupportsLogStyles(), false, 'is false')
+
+  t.end()
+})
+
+test('browserSupportsLogStyles in WebKit browser', function (t) {
+  resetBrowserEnvironment({
+    document: {
+      documentElement: {
+        style: {
+          WebkitAppearance: 'foo'
+        }
+      }
+    }
+  })
+
+  t.is(browserSupportsLogStyles(), true, 'is true')
+
+  t.end()
+})
+
+test('browserSupportsLogStyles in Firefox 30', function (t) {
+  resetBrowserEnvironment({
+    navigator: {
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:30.0) Gecko/20100101 Firefox/30.0'
+    }
+  })
+
+  t.is(browserSupportsLogStyles(), false, 'is false')
+
+  t.end()
+})
+
+test('browserSupportsLogStyles in Firefox 31', function (t) {
+  resetBrowserEnvironment({
+    navigator: {
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0'
+    }
+  })
+
+  t.is(browserSupportsLogStyles(), true, 'is true')
+
+  t.end()
+})
+
+test('browserSupportsLogStyles with firebug', function (t) {
+  resetBrowserEnvironment({
+    window: {
+      console: {
+        firebug: {}
+      }
+    }
+  })
+
+  t.is(browserSupportsLogStyles(), true, 'is true if console.firebug is set')
+
+  resetBrowserEnvironment({
+    window: {
+      console: {
+        exception: function () {},
+        table: function () {}
+      }
+    }
+  })
+
+  t.is(browserSupportsLogStyles(), true, 'is true if console.exception & .table are functions')
+
+  t.end()
+})
