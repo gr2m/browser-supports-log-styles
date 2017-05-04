2e002b1bc1aef0523866c7585c90f8944be8cf91
Oleg Solomka <legomushroom@gmail.com>
2017-05-04 14:32:36 -0500

    [edge browsers]: filter out by ua

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function browserSupportsLogStyles () {
     return false
   }
 
+  // edge browser? https://msdn.microsoft.com/en-us/library/hh869301%28v=vs.85%29.aspx
+  var isEdge = navigator.userAgent.toLowerCase().indexOf('edge') > -1
   // http://stackoverflow.com/a/16459606/376773
   var isWebkit = 'WebkitAppearance' in document.documentElement.style
   // http://stackoverflow.com/a/398120/376773
@@ -13,5 +15,5 @@ function browserSupportsLogStyles () {
   // firefox >= v31? https://developer.mozilla.org/en-US/docs/Tools/Web_Console#Styling_messages
   var isFirefoxWithLogStyleSupport = navigator.userAgent.toLowerCase().match(/firefox\/(\d+)/) && parseInt(RegExp.$1, 10) >= 31
 
-  return isWebkit || isFirebug || isFirefoxWithLogStyleSupport || false
+  return (isWebkit && !isEdge) || isFirebug || isFirefoxWithLogStyleSupport || false
 }
